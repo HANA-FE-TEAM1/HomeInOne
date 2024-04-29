a86662e4d3d6e44191405c43d27a688c08711c12
채채 <109331665+cherrytonic@users.noreply.github.com>
2024-04-29 18:51:22 +0900

    feat: test

--- javascript/switch.js
+++ javascript/switch.js
@@ -1,29 +1,30 @@
-const airconSwitch = document.getElementById('aircon-power')
-const humidifierSwitch = document.getElementById('humid-power')
-const refridgeratorSwitch = document.getElementById('refridge-power')
-const washingMachineSwitch = document.getElementById('washing-power')
 
 // 각 스위치에 대응하는 로컬 스토리지의 값을 가져와서 체크 여부를 설정.
-window.addEventListener('DOMContentLoaded', () => {
-    airconSwitch.checked = localStorage.getItem('power3') === 'true';
-    humidifierSwitch.checked = localStorage.getItem('power0') === 'true';
-    refridgeratorSwitch.checked = localStorage.getItem('power1') === 'true';
-    washingMachineSwitch.checked = localStorage.getItem('power2') === 'true';
-});
-airconSwitch.addEventListener('click', (e) => {
-    const power1 = airconSwitch.checked ? 'true' : 'false';
-    localStorage.setItem('power3', power1);
-})
-humidifierSwitch.addEventListener('click', (e) => {
-    const power2 = humidifierSwitch.checked ? 'true' : 'false';
-    localStorage.setItem('power0', power2);
-})
-refridgeratorSwitch.addEventListener('click', (e) => {
-    const power3 = refridgeratorSwitch.checked ? 'true' : 'false';
-    localStorage.setItem('power1', power3);
-})
-washingMachineSwitch.addEventListener('click', (e) => {
-    const power4 = washingMachineSwitch.checked ? 'true' : 'false';
-    localStorage.setItem('power2', power4);
-})
+const airconSwitch0 = document.getElementById('aircon-power')
+const humidifierSwitch0 = document.getElementById('humid-power')
+// const humidifierSwitch = document.querySelector('#container>.switch-container>.switch>#humid-power')
+const refridgeratorSwitch0 = document.getElementById('refridge-power')
+const washingMachineSwitch0 = document.getElementById('washing-power')
 
+window.addEventListener('DOMContentLoaded', () => {
+    airconSwitch0.checked = localStorage.getItem('power3') === 'true';
+    humidifierSwitch0.checked = localStorage.getItem('power0') === 'true';
+    refridgeratorSwitch0.checked = localStorage.getItem('power1') === 'true';
+    washingMachineSwitch0.checked = localStorage.getItem('power2') === 'true';
+});
+function airconOn() {
+    const power1 = airconSwitch0.checked ? 'true' : 'false';
+    localStorage.setItem('power3', power1);
+}
+function humidOn() {
+    const power2 = humidifierSwitch0.checked ? 'true' : 'false';
+    localStorage.setItem('power0', power2);
+}
+function refridgeOn() {
+    const power3 = refridgeratorSwitch0.checked ? 'true' : 'false';
+    localStorage.setItem('power1', power3);
+}
+function washingOn() {
+    const power4 = washingMachineSwitch0.checked ? 'true' : 'false';
+    localStorage.setItem('power2', power4);
+}
